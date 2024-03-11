ddf1457adb67a5e0e8e74f30f8f42b7f8e8180f0
myxi <myxi@envs.net>
2024-03-12 01:54:26 +0530

    feat: merged relevant plugin files into one

--- .config/nvim/lua/plugins/theming.lua
+++ .config/nvim/lua/plugins/theming.lua
@@ -1,4 +1,5 @@
 return {
+    -- color scheme
     {
         "morhetz/gruvbox",
         priority = 1000,
@@ -6,5 +7,69 @@ return {
             vim.cmd.colorscheme("gruvbox")
             vim.cmd("hi Normal guibg=#282828")
         end,
+    },
+    -- colorized delimiters
+    {
+        "hiphish/rainbow-delimiters.nvim",
+        init = function()
+            vim.g.rainbow_delimiters = {
+                highlight = {
+                    "GruvboxOrange",
+                    "GruvboxYellow",
+                    "GruvboxBlue",
+                    "GruvboxGreen",
+                    "GruvboxViolet",
+                    "GruvboxCyan",
+                    "GruvboxRed",
+                },
+            }
+        end,
+    },
+    -- colorized comments
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {
+            keywords = {
+                TODO = {
+                    icon = " ",
+                    color = "Green",
+                },
+                HACK = {
+                    icon = " ",
+                    color = "Red",
+                },
+                WARN = {
+                    icon = " ",
+                    color = "Red",
+                    alt = { "WARNING", "XXX" },
+                },
+                PERF = {
+                    icon = " ",
+                    color = "Red",
+                    alt = {
+                        "OPTIM",
+                        "PERFORMANCE",
+                        "OPTIMIZE",
+                    },
+                },
+                NOTE = {
+                    icon = " ",
+                    color = "Yellow",
+                    alt = { "INFO" },
+                },
+                TEST = {
+                    icon = "⏲ ",
+                    color = "Blue",
+                    alt = { "TESTING", "PASSED", "FAILED" },
+                },
+            },
+            colors = {
+                Red = { "GruvboxRed" },
+                Green = { "GruvboxGreen" },
+                Yellow = { "GruvboxYellow" },
+                Blue = { "GruvboxBlue" },
+            },
+        },
     },
 }
