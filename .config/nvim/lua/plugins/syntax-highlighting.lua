66b27d204328c722763ad8722179ce40d5e0016d
myxi <myxi@envs.net>
2024-03-12 01:21:16 +0530

    feat: organized plugins

---
+++ .config/nvim/lua/plugins/syntax-highlighting.lua
@@ -0,0 +1,40 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "c",
+                "javascript",
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "query",
+                "vim",
+                "vimdoc",
+            },
+            sync_install = true,
+            auto_install = true,
+            highlight = {
+                enable = true,
+                disable = { "c", "rust" },
+                disable = function(lang, buf)
+                    local max_filesize = 100 * 1024
+                    local ok, stats = pcall(
+                        vim.loop.fs_stat,
+                        vim.api.nvim_buf_get_name(buf)
+                    )
+                    if
+                        ok
+                        and stats
+                        and stats.size > max_filesize
+                    then
+                        return true
+                    end
+                end,
+                additional_vim_regex_highlighting = false,
+            },
+        })
+    end,
+}
