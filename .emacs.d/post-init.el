f3b88ba8e5c748f94916d53d597f4a922a48943f
myxi <myxi@envs.net>
2024-12-18 17:19:35 +0530

    feat: improvements

--- .emacs.d/post-init.el
+++ .emacs.d/post-init.el
@@ -45,22 +45,53 @@
           (lambda () 
             (whitespace-newline-mode t)))
 
+(global-unset-key (kbd "M-'"))
+
 ;; INFO: packages
-(require 'my-meow)
-
-(use-package meow-tree-sitter
+(use-package multiple-cursors
+  :after boon
   :ensure t
+  :bind (:map boon-command-map
+              (("C->" . 'mc/mark-next-like-this)
+               ("C-<" . 'mc/mark-previous-like-this)
+               ("C-S-c C-S-c" . 'mc/edit-lines)
+               ("C-c C-<" . 'mc/mark-all-like-this))))
+
+(use-package which-key :ensure t :config (which-key-mode))
+
+(use-package boon 
+  :ensure t 
   :config
-  (unless (featurep 'meow-tree-sitter-configured)
-    (meow-tree-sitter-register-defaults)
-    (provide 'meow-tree-sitter-configured)))
+  (require 'boon-colemak-hnei)
+  (boon-mode)
 
-(require 'emacs-surround)
-(add-to-list 'emacs-surround-alist '("*" . ("**" . "**")))
-(add-to-list 'emacs-surround-alist '("8" . ("*" . "**")))
-(global-set-key (kbd "C-c C-SPC C-s") 'emacs-surround)
+  ;; https://github.com/meow-edit/meow/discussions/168#discussioncomment-2974816
+  (setq boon-two-char-escape-sequence "kt")
+  (setq boon-two-char-escape-delay 0.2)
 
-(use-package goto-chg :ensure t)
+  (defun boon--two-char-exit-insert-state (s)
+    (when boon-insert-state
+      (let ((modified (buffer-modified-p)))
+        (insert (elt s 0))
+        (let* ((second-char (elt s 1))
+               (event
+                (if defining-kbd-macro
+                    (read-event nil nil)
+                  (read-event nil nil boon-two-char-escape-delay))))
+          (when event
+            (if (and (characterp event) (= event second-char))
+                (progn
+                  (backward-delete-char 1)
+                  (set-buffer-modified-p modified)
+                  (boon-set-command-state))
+              (push event unread-command-events)))))))
+
+  (defun boon-two-char-exit-insert-state ()
+    (interactive)
+    (boon--two-char-exit-insert-state boon-two-char-escape-sequence))
+
+  (define-key boon-insert-map (substring boon-two-char-escape-sequence 0 1)
+              #'boon-two-char-exit-insert-state))
 
 (use-package dumb-jump
   :ensure t
@@ -69,10 +100,6 @@
   (setq xref-show-definitions-function #'xref-show-definitions-completing-read))
 
 (use-package ag :ensure t)
-
-(use-package expand-region
-  :ensure t
-  :bind ("C-;" . 'er/expand-region))
 
 (use-package ligature
   :config
@@ -93,7 +120,7 @@
   (global-ligature-mode t))
 
 (use-package undo-tree
-  :defer t
+  :ensure t
   :diminish undo-tree-mode
   :config
   (global-undo-tree-mode)
@@ -121,7 +148,7 @@
   (dtrt-indent-global-mode t))
 
 (use-package titlecase
-  :bind ("C-c C-SPC T" . titlecase-region)
+  :bind ("M-' T" . titlecase-region)
   :ensure t)
 
 (use-package prescient
@@ -153,13 +180,13 @@
 
 (use-package eglot
   :ensure t
-  :bind (("C-c C-SPC C-l l" . eglot)
-         ("C-c C-SPC C-l s" . eglot-shutdown)
-         ("C-c C-SPC C-l d" . eldoc)
-         ("C-c C-SPC C-l r" . eglot-rename)
-         ("C-c C-SPC C-l R" . eglot-reconnect)
-         ("C-c C-SPC C-l f" . eglot-format-buffer)
-         ("C-c C-SPC C-l i" . eglot-code-action-organize-imports))
+  :bind (("M-' l l" . eglot)
+         ("M-' l s" . eglot-shutdown)
+         ("M-' l d" . eldoc)
+         ("M-' l r" . eglot-rename)
+         ("M-' l R" . eglot-reconnect)
+         ("M-' l f" . eglot-format-buffer)
+         ("M-' l i" . eglot-code-action-organize-imports))
   :commands (eglot
              eglot-rename
              eglot-format-buffer
@@ -188,28 +215,28 @@
   (add-hook 'emacs-lisp-mode-hook #'aggressive-indent-mode)
   (add-hook 'css-mode-hook #'aggressive-indent-mode))
 
-(use-package centaur-tabs
-  :ensure t
-  :bind ("C-c C-SPC C-k t" . centaur-tabs-ace-jump)
-  :demand
-  :config
-  (setq-default centaur-tabs-set-bar 'right)
-  (setq-default centaur-tabs-style "slant")
-  (setq-default centaur-tabs-ace-jump-keys
-                '(?a ?r ?s ?t ?g ?m ?n ?e ?i ?c ?k ?m ?w ?y))
-  (centaur-tabs-mode t)
-  (custom-set-faces
-   '(centaur-tabs-default
-     ((t (:background "#282828" :foreground "#928374"))))
-   '(centaur-tabs-unselected
-     ((t (:background "#282828" :foreground "#a89984"))))
-   '(centaur-tabs-selected
-     ((t (:background "#504945" :foreground "#ebdbb2"))))
-   '(centaur-tabs-unselected-modified
-     ((t (:background "#3c3836" :foreground "#fabd2f"))))
-   '(centaur-tabs-selected-modified
-     ((t (:background "#504945" :foreground "#fabd2f")))))
-  (centaur-tabs-headline-match))
+;; (use-package centaur-tabs
+;;   :ensure t
+;;   :bind ("C-c C-SPC C-k t" . centaur-tabs-ace-jump)
+;;   :demand
+;;   :config
+;;   (setq-default centaur-tabs-set-bar 'right)
+;;   (setq-default centaur-tabs-style "slant")
+;;   (setq-default centaur-tabs-ace-jump-keys
+;;                 '(?a ?r ?s ?t ?g ?m ?n ?e ?i ?c ?k ?m ?w ?y))
+;;   (centaur-tabs-mode t)
+;;   (custom-set-faces
+;;    '(centaur-tabs-default
+;;      ((t (:background "#282828" :foreground "#928374"))))
+;;    '(centaur-tabs-unselected
+;;      ((t (:background "#282828" :foreground "#a89984"))))
+;;    '(centaur-tabs-selected
+;;      ((t (:background "#504945" :foreground "#ebdbb2"))))
+;;    '(centaur-tabs-unselected-modified
+;;      ((t (:background "#3c3836" :foreground "#fabd2f"))))
+;;    '(centaur-tabs-selected-modified
+;;      ((t (:background "#504945" :foreground "#fabd2f")))))
+;;   (centaur-tabs-headline-match))
 
 (use-package hydra :ensure t)
 
@@ -232,7 +259,6 @@
   :custom ((snap-indent-format 'untabify)
            (snap-indent-on-save t)))
 
-
 (use-package company
   :ensure t
   :config
@@ -250,25 +276,29 @@
 
 (use-package exec-path-from-shell
   :ensure t
-  ;; :init
-  ;; (setq exec-path-from-shell-arguments nil)
   :config
   (when (daemonp)
     (exec-path-from-shell-initialize)))
 
 (use-package avy
   :ensure t
-  :bind (("C-c C-SPC C-n t" . avy-goto-char-2)
-         ("C-c C-SPC C-n T" . avy-goto-char-timer))
+  :bind (:map boon-command-map
+              ("o" . my/avy-goto-char-2-end)
+              ("O" . avy-goto-char))        
   :custom
   (avy-all-windows nil)
   (avy-all-windows-alt t)
   (avy-background t)
   (avy-style 'de-bruijn)
-  (avy-keys (string-to-list "arstneiofu")))
-
-(use-package conf-mode
-  :bind (:map conf-space-mode-map ("C-c SPC" . nil)))
+  (avy-keys (string-to-list "arstneiofu"))
+  :config
+  (defun my/avy-goto-char-2-end ()
+    "Move to the end of the match for avy-goto-char-2."
+    (interactive)
+    (let ((char1 (read-char "First char: "))
+          (char2 (read-char "Second char: ")))
+      (avy-goto-char-2 char1 char2)
+      (forward-char 2))))
 
 (use-package shift-number
   :ensure t)
@@ -282,7 +312,7 @@
   :config
   (projectile-mode +1)
   :bind (:map projectile-mode-map
-              ("M-'" . projectile-command-map)))
+              ("M-' p" . projectile-command-map)))
 
 (use-package zoom
   :ensure t
@@ -293,15 +323,15 @@
   :config)
 
 (use-package swiper
-  :bind (("C-c C-SPC C-n I" . swiper-isearch)))
+  :bind (:map boon-command-map ("p" . swiper)))
 
 (use-package counsel
   :ensure t
   :after flx
-  :bind (("C-c C-SPC C-n a" . counsel-grep-or-swiper)
-         ("C-c C-SPC C-n i" . counsel-file-jump)
-         ("C-c C-SPC C-n n" . counsel-bookmark)
-         ("C-c C-SPC C-n f" . counsel-buffer-or-recentf))
+  :bind (("M-' n a" . counsel-grep-or-swiper)
+         ("M-' n i" . counsel-file-jump)
+         ("M-' n n" . counsel-bookmark)
+         ("M-' n f" . counsel-buffer-or-recentf))
   :bind (("C-x C-f" . counsel-find-file)
          ("M-y"     . counsel-yank-pop)
          ("M-x"     . counsel-M-x)
@@ -328,10 +358,15 @@
 (add-hook 'after-init-hook #'savehist-mode)
 (add-hook 'after-init-hook #'save-place-mode)
 
-;; load history when loading a buffer
+(defun my-on-boon-command-state-change (symbol new-value operation where)
+  "Run something whenever `boon-command-state` is set to t."
+  (when (and (eq symbol 'boon-command-state)
+             (eq new-value t))
+    (save-some-buffers t)))
+(add-variable-watcher 'boon-command-state #'my-on-boon-command-state-change)
+
 (add-hook 'find-file-hook (lambda ()
                             (undo-tree-load-history nil t)))
-
 ;; save history on exit
 (add-hook 'kill-emacs-query-functions (lambda ()
                                         (undo-tree-save-history nil 'overwrite)))
@@ -346,11 +381,6 @@
             (when (and (buffer-file-name) undo-tree-mode)
               (undo-tree-save-history nil 'overwrite))))
 
-;; 
-;; save buffer when leaving insert mode
-(add-hook 'meow-insert-exit-hook (lambda ()
-                                   (save-some-buffers t)))
-
 
 ;; INFO: additional code
 (require 'funcs)
@@ -361,9 +391,6 @@
 (add-to-list 'pulsar-pulse-functions #'avy-goto-char-timer)
 (add-to-list 'pulsar-pulse-functions #'avy-goto-char-2)
 
-;; INFO: keymaps
-(define-key global-map (kbd "C-,") 'my-duplicate-line)
-
 (defun my/backward-delete-word ()
   "Delete the previous word without saving to the kill ring."
   (interactive)
@@ -371,55 +398,45 @@
         (end (progn (backward-word) (point))))
     (delete-region start end)))
 
-(meow-define-keys
-    'insert
-  '("C-<backspace>" . my/backward-delete-word)
-  '("M-;" . avy-goto-char-2))
+;; INFO: keymaps
+(define-key global-map (kbd "C-<backspace>") 'my/backward-delete-word)
+(define-key global-map (kbd "C-,") 'my-duplicate-line)
 
-(meow-normal-define-key
- '("C-<left>"  . centaur-tabs-backward)
- '("C-<right>" . centaur-tabs-forward)
- '("C-i"       . goto-last-change)
- '("C-S-i"     . goto-last-change-reverse)
- '("M-;"       . avy-goto-char-2)
- '("V"         . call-last-kbd-macro))
+(define-key global-map (kbd "M-' q") #'quit-window)
+(define-key global-map (kbd "M-' s") #'boon-enclose)
+(define-key global-map (kbd "M-' TAB") #'mode-line-other-buffer)
+(define-key global-map (kbd "M-' f") #'dired-jump)
+(define-key global-map (kbd "M-' Q") #'kill-emacs)
+(define-key global-map (kbd "M-' y") (lambda () (interactive)
+                                       (find-file (expand-file-name "post-init.el" user-emacs-directory))))
+(define-key global-map (kbd "M-' Y") (lambda () (interactive) (load-file user-init-file)))
+(define-key global-map (kbd "M-' u s") #'query-replace)
+(define-key global-map (kbd "M-' u S") #'query-replace-regexp)
 
-(meow-leader-define-key
- '("C-SPC y"     . (lambda () (interactive)
-                     (find-file (expand-file-name "post-init.el" user-emacs-directory))))
- '("C-SPC Y"     . (lambda () (interactive) (load-file user-init-file)))
- '("SPC"         . "C-c C-SPC")
- '("C-SPC SPC"   . "H-SPC")
- '("C-SPC f"     . dired-jump)
- '("C-SPC q"     . quit-window)
- '("C-SPC Q"     . kill-emacs)
- '("C-SPC t"     . hydra-paging/body)
- '("C-SPC TAB"   . mode-line-other-buffer)
- '("C-SPC C-w n" . windmove-down)
- '("C-SPC C-w e" . windmove-up)
- '("C-SPC C-w h" . windmove-right)
- '("C-SPC C-w i" . windmove-left)
- '("C-SPC C-w y" . delete-window)
- '("C-SPC C-c y" . spacemacs/show-hide-compilation-window)
- '("C-SPC C-c s" . spacemacs/switch-to-compilation-buffer)
- '("C-SPC C-c c" . compile)
- '("C-SPC C-c r" . recompile)
- '("C-SPC C-c n" . next-error)
- '("C-SPC C-c e" . previous-error)
- '("C-SPC C-c k" . kill-compilation)
- '("C-SPC C-u u" . delete-trailing-whitespace)
- '("C-SPC C-u a" . align-regexp)
- '("C-SPC C-u n" . hydra-numbers/body)
- '("C-SPC C-u n" . hydra-numbers/body)
- '("C-SPC C-u t" . hydra-todo/body)
- '("C-SPC C-u h" . evil-ex-nohighlight)
- '("C-SPC C-u s" . query-replace)
- '("C-SPC C-u S" . query-replace-regexp)
- '("C-SPC C-u z" . hydra-zoom/body)
- '("C-SPC C-k p" . kill-this-buffer)
- '("C-SPC C-k P" . kill-buffer)
- '("C-SPC C-k T" . bookmark-delete)
- '("C-SPC C-k m" . spacemacs/switch-to-messages-buffer)
- '("C-SPC C-k s" . spacemacs/switch-to-scratch-buffer)
- )
+(define-key global-map (kbd "M-' w n") #'windmove-down)
+(define-key global-map (kbd "M-' w e") #'windmove-up) 
+(define-key global-map (kbd "M-' w h") #'windmove-right)
+(define-key global-map (kbd "M-' w i") #'windmove-left)
+(define-key global-map (kbd "M-' w y") #'windmove-left)
 
+(define-key global-map (kbd "M-' c y") #'spacemacs/show-hide-compilation-window)
+(define-key global-map (kbd "M-' c s") #'spacemacs/switch-to-compilation-buffer)
+(define-key global-map (kbd "M-' c c") #'compile)
+(define-key global-map (kbd "M-' c r") #'recompile)
+(define-key global-map (kbd "M-' c n") #'next-error)
+(define-key global-map (kbd "M-' c e") #'previous-error)
+(define-key global-map (kbd "M-' c k") #'kill-compilation)
+
+(define-key global-map (kbd "M-' u u") #'delete-trailing-whitespace)
+(define-key global-map (kbd "M-' u a") #'align-regexp)
+
+(define-key global-map (kbd "M-' k p") #'kill-this-buffer)
+(define-key global-map (kbd "M-' k P") #'kill-buffer)
+(define-key global-map (kbd "M-' k T") #'bookmark-delete)
+(define-key global-map (kbd "M-' k m") #'spacemacs/switch-to-messages-buffer)
+(define-key global-map (kbd "M-' k s") #'spacemacs/switch-to-scratch-buffer)
+
+(define-key global-map (kbd "M-' h t") #'hydra-paging/body)
+(define-key global-map (kbd "M-' h n") #'hydra-numbers/body)
+(define-key global-map (kbd "M-' h T") #'hydra-todo/body)
+(define-key global-map (kbd "M-' h z") #'hydra-zoom/body)
--- docs/emacs.md
+++ docs/emacs.md
@@ -7,6 +7,7 @@ framework so a subset of the shortcuts listed here are from that framework.
 - Update (24 November 2024): I switched from [Evil
 Mode](https://github.com/emacs-evil/evil "Evil Modal Editing") to [Meow
 Mode](https://github.com/meow-edit/meow "Meow Modal Editing").
+- Update (18 November 2024): I switched from [Meow Mode](https://github.com/meow-edit/meow) to [Boon Mode](https://github.com/jyp/boon).
 
 # General
 - `M-x s d` see the diff of the current buffer and its corresponding file
@@ -16,61 +17,61 @@ Mode](https://github.com/meow-edit/meow "Meow Modal Editing").
 - `C-u M-!` insert output of a shell command into buffer
 
 # Editing and Surrounding
-- `C-c C-SPC C-s` add or remove surrounding text
-- `C-c C-SPC T` apply title case to a region
-- `C-c C-SPC C-u u` remove trailing whitespace
-- `C-c C-SPC C-u a` align by regular expression
-- `C-c C-SPC C-u s` perform a find-and-replace
-- `C-c C-SPC C-u S` perform a regex find-and-replace
+- `M-' s` add or remove surrounding text
+- `M-' T` apply title case to a region
+- `M-' u u` remove trailing whitespace
+- `M-' u a` align by regular expression
+- `M-' u s` perform a find-and-replace
+- `M-' u S` perform a regex find-and-replace
 
 # Navigation
-- `C-c C-SPC C-n t` jump to a character pair using `avy`
-- `C-c C-SPC C-n T` jump with a character timer using `avy`
-- `C-c C-SPC C-n I` search text using `swiper`
-- `C-c C-SPC C-n a` search or grep using `counsel`
-- `C-c C-SPC C-n i` jump to files using `counsel`
-- `C-c C-SPC C-n n` manage bookmarks using `counsel`
-- `C-c C-SPC C-n f` open recent files or buffers using `counsel`
+- `M-' n t` jump to a character pair using `avy`
+- `M-' n T` jump with a character timer using `avy`
+- `M-' n I` search text using `swiper`
+- `M-' n a` search or grep using `counsel`
+- `M-' n i` jump to files using `counsel`
+- `M-' n n` manage bookmarks using `counsel`
+- `M-' n f` open recent files or buffers using `counsel`
 
 # File and Buffer Management
-- `C-c C-SPC f` open file in Dired
-- `C-c C-SPC y` edit `post-init.el` file
-- `C-c C-SPC Y` reload configuration
-- `C-c C-SPC q` quit window
-- `C-c C-SPC Q` exit Emacs
-- `C-c C-SPC TAB` switch to the previous buffer
-- `C-c C-SPC C-k p` close current buffer
-- `C-c C-SPC C-k P` kill another buffer
-- `C-c C-SPC C-k T` delete a bookmark
-- `C-c C-SPC C-k m` open messages buffer
-- `C-c C-SPC C-k s` switch to scratch buffer
+- `M-' f` open file in Dired
+- `M-' y` edit `post-init.el` file
+- `M-' Y` reload configuration
+- `M-' q` quit window
+- `M-' Q` exit Emacs
+- `M-' TAB` switch to the previous buffer
+- `M-' k p` close current buffer
+- `M-' k P` kill another buffer
+- `M-' k T` delete a bookmark
+- `M-' k m` open messages buffer
+- `M-' k s` switch to scratch buffer
 
 # Window Management
-- `C-c C-SPC C-w n` move window focus downward
-- `C-c C-SPC C-w e` move window focus upward
-- `C-c C-SPC C-w h` move window focus rightward
-- `C-c C-SPC C-w i` move window focus leftward
-- `C-c C-SPC C-w y` delete window
+- `M-' w n` move window focus downward
+- `M-' w e` move window focus upward
+- `M-' w h` move window focus rightward
+- `M-' w i` move window focus leftward
+- `M-' w y` delete window
 
 # Compilation and Error Navigation
-- `C-c C-SPC C-c y` toggle compilation window
-- `C-c C-SPC C-c s` switch to compilation buffer
-- `C-c C-SPC C-c c` compile
-- `C-c C-SPC C-c n` jump to the next error
-- `C-c C-SPC C-c e` jump to the previous error
-- `C-c C-SPC C-c k` kill compilation
+- `M-' c y` toggle compilation window
+- `M-' c s` switch to compilation buffer
+- `M-' c c` compile
+- `M-' c n` jump to the next error
+- `M-' c e` jump to the previous error
+- `M-' c k` kill compilation
 
 # Language Server Protocol (LSP)
-- `C-c C-SPC C-l l` start the `eglot` LSP client
-- `C-c C-SPC C-l s` stop `eglot`
-- `C-c C-SPC C-l d` show documentation with `eldoc`
-- `C-c C-SPC C-l r` rename using `eglot`
-- `C-c C-SPC C-l R` reconnect `eglot`
-- `C-c C-SPC C-l f` format buffer with `eglot`
-- `C-c C-SPC C-l i` organize imports with `eglot`
+- `M-' l l` start the `eglot` LSP client
+- `M-' l s` stop `eglot`
+- `M-' l d` show documentation with `eldoc`
+- `M-' l r` rename using `eglot`
+- `M-' l R` reconnect `eglot`
+- `M-' l f` format buffer with `eglot`
+- `M-' l i` organize imports with `eglot`
 
 # Custom and Miscellaneous Commands
-- `C-c C-SPC t` use custom paging commands
-- `C-c C-SPC C-u n` open number editing Hydra
-- `C-c C-SPC C-u t` open TODO management Hydra
-- `C-c C-SPC C-u h` clear search highlights
+- `M-' h t` use custom paging commands
+- `M-' h n` open number editing Hydra
+- `M-' h T` open TODO management Hydra
+- `M-' h h` clear search highlights
